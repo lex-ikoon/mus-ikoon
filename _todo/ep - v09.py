0315208779e49025c49c8b0f426bc489891de2b6
Jiri Miratsky <info@ikoon.cz>
2023-10-09 08:32:50 +0200

    keycurve interpolate

--- _todo/ep - v09.py
+++ _todo/ep - v09.py
@@ -4,7 +4,18 @@
 ##########################################################
 
 @ jak vyresit barevnost
-@ vsechny xe lines = maji svoje id? NE
+    - colorize table
+    - random along curves:
+            hue
+            saturation
+            value
+    - random at start: HSV
+    - random at end: HSV
+    - similar targetting as clumping?
+
+
+    
+@ vsechny xe lines = maji svoje id? ANO
 @ vsechny xe lines = maji grouped first/last points? 
 
 
@@ -14,8 +25,10 @@
 
 def "xe // curves / connector"
 
-    NO resample
+    resample by asi mohl byt?
+    b-spline? nebo subdiv curves
     NO smooth
+
 
     output polyline #ma vyhodu, ze muze mit podobny sampling jako targety
 
@@ -117,68 +130,12 @@ def "xf // animate / fill"
 
 
 ##########################################################
-#############           TARGET             ###############
-##########################################################
-
-
-def "xf // target"
-
-    uvnitr je timeshift
-
-    parm:
-        spread_smooth timeshift?
-
-    prim parametry:
-        @dim
-        @coord
-        @spread
-
-
-
-
-
-##########################################################
 #############           SAMPLER            ###############
 ##########################################################
 
 
 
 def "xg // sampler"
-
-    asi na tom muze byt help
-    AUTO / COORD
-
-    parm:
-        tog / first target is connector (to se zjisti z prvniho targetu)
-        ------------
-        int / pocet id #to musi byt vsude stejne
-        ------------
-        tog / output line #pokud ne, tak je to jen bod
-        int / divisions
-
-    uvnitr:
-
-
-    VEX (over points):
-        - najde vsechny targety (0-2-4 nebo 1-3)
-        - na vsech najde coord
-        - NE pocitam s tim, ze coord je 0-1 pouze na dvou targetech
-        - spocita se COORD podel cele lajny
-            - proste soucet vsech COORD
-            # - vazeny prumer kazdeho targetu ... vaha je nizka u koncu
-            # - kdyz je vedle sebe 1 a 0 ??? ... tak by u connection melo byt 0.5
-
-        - musime zjistit, kam patri tento bod v ramci divisions
-            - bod ma svoje @curveu
-            - zjistime fill_to_coord (od-do)
-        
-        - samplujeme pozici
-            - vzdy se sampluji dva sousedni targety, kvuli spread_smooth
-            - podle toho, jak moc 
-
-        # - dopocita se coord na vsech targetech
-        #     - prev targetu (pokud < 0.5 )
-        #     - next targetu (pokud > 0.5 )
 
 
 
