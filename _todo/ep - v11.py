84043dbfbc7ac832913a7444be5d135a20bd0f93
Jiri Miratsky <info@ikoon.cz>
2023-10-11 07:41:14 +0200

    connector + interpolate vex

---
+++ _todo/ep - v11.py
@@ -0,0 +1,81 @@
+
+##########################################################
+#############            OTAZKY            ###############
+##########################################################
+
+@ jak vyresit barevnost
+    - colorize table
+    - random along curves:
+            hue
+            saturation
+            value
+    - random at start: HSV
+    - random at end: HSV
+    - similar targetting as clumping?
+
+
+    
+@ vsechny xe lines = maji svoje id? ANO
+@ vsechny xe lines = maji grouped first/last points? 
+
+
+##########################################################
+#############    ABOVE TARGET nastroje     ###############
+##########################################################
+
+def "xe // curves / connector"
+
+    # resample by asi mohl byt?
+    # b-spline? nebo subdiv curves
+    # NO smooth
+
+
+-------------------------------------------------------------------
+
+
+def "xe // curves / connector"
+def "xe // curves / divergence trails" 
+def "xe // curves / eps"
+def "xe // curves / from points"      # three points
+def "xe // curves / from trails" # cam/pop = musime prevest na DIM + COORD + SPREAD)
+def "xe // curves / intersect planes" #cut in some direction
+def "xe // curves / multiline sweep"
+def "xe // curves / silhouettes"
+def "xe // curves / wrap"
+
+
+-------------------------------------------------------------------
+
+def "xe // edit / clumping"
+    # podle id, misto... na zacatku / na konci)
+
+
+def "xe // edit / reverse detector"
+    # podle blizkosti k bodu, podle vektoru?)
+    # podle id?
+
+
+def "xe // edit / match id"
+    # optimal transport
+    # 2nd input - previous target
+
+
+-------------------------------------------------------------------
+
+def "xf // animate / dim"
+
+def "xf // animate / fill"
+
+
+
+##########################################################
+#############           SAMPLER            ###############
+##########################################################
+
+
+
+def "xg // sampler"
+
+
+
+
